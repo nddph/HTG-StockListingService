5846c77a11125f286dfa34eeeba2409c0a7eb090
mh <man.hx@softworld.vn>
2022-02-14 09:28:27 +0700

    thêm api đếm ticket

--- StockDealBusiness/RequestDB/TicketDB.cs
+++ StockDealBusiness/RequestDB/TicketDB.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StockDealDal.Dto.Ticket;
 using StockDealDal.Entities;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,73 @@ namespace StockDealBusiness.RequestDB
 {
     public class TicketDB
     {
+        public string GetListTicketQuery(TicketSearchCriteria listTicketDto, Guid loginContactId)
+        {
+            return string.Format(@"EXECUTE [GetListTickets] @ticketType = {0},
+                        @stockCodes = N'{1}', @status = {2}, @ownerId = '{3}', @byUserType = {4}, @priceFrom = {5}, @priceTo = {6},
+                        @quantityFrom = {7}, @quantityTo = {8}, @byNewer = {9},
+                        @expTicketStatus = {10}, @delTicketStatus = {11},
+                        @currentPage = {12}, @pageSize = {13}, @quantityStatus = {14}, @searchText = N'{15}', @orderByPriceType = {16},
+                        @stockTypeIds = N'{17}', @isPaging = {18}",
+                        listTicketDto.TicketType,
+                        string.Join(",", listTicketDto.StockCodes),
+                        listTicketDto.Status,
+                        loginContactId,
+                        listTicketDto.ByUserType,
+                        listTicketDto.PriceFrom, listTicketDto.PriceTo,
+                        listTicketDto.QuantityFrom, listTicketDto.QuantityTo,
+                        listTicketDto.ByNewer ? 1 : 0,
+                        listTicketDto.ExpTicketStatus,
+                        listTicketDto.DelTicketStatus,
+                        listTicketDto.CurrentPage,
+                        listTicketDto.PerPage,
+                        listTicketDto.QuantityStatus,
+                        (listTicketDto.SearchText ?? "").Trim(),
+                        listTicketDto.OrderByPriceType,
+                        string.Join(",", listTicketDto.StockTypeIds),
+                        listTicketDto.IsPaging ? 1 : 0
+                        );
+        }
+
+
+
+        /// <summary>
+        /// lấy danh sách tin mua
+        /// </summary>
+        /// <param name="listTicketDto"></param>
+        /// <param name="loginContactId"></param>
+        /// <returns></returns>
+        public static async Task<List<ViewSaleTickets>> ListSaleTicketAsync(TicketSearchCriteria listTicketDto, Guid loginContactId)
+        {
+            var ticketDb = new TicketDB();
+            var context = new StockDealServiceContext();
+
+            var sql = ticketDb.GetListTicketQuery(listTicketDto, loginContactId);
+
+            return await context.ViewSaleTickets.FromSqlRaw(sql).AsNoTracking().ToListAsync();
+        }
+
+
+
+        /// <summary>
+        /// Lấy danh sách tin bán
+        /// </summary>
+        /// <param name="listTicketDto"></param>
+        /// <param name="loginContactId"></param>
+        /// <param name="isBuyTicket"></param>
+        /// <returns></returns>
+        public static async Task<List<ViewBuyTickets>> ListBuyTicketAsync(TicketSearchCriteria listTicketDto, Guid loginContactId)
+        {
+            var ticketDb = new TicketDB();
+            var context = new StockDealServiceContext();
+
+            var sql = ticketDb.GetListTicketQuery(listTicketDto, loginContactId);
+
+            return await context.ViewBuyTickets.FromSqlRaw(sql).AsNoTracking().ToListAsync();
+        }
+
+
+
         /// <summary>
         /// Xóa tin mua bán
         /// nếu isAll bằng true thì xóa tất cả, ngược lại thì xóa đổi với các ticket có id trong listTicketId
