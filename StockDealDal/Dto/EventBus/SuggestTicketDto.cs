e7989c720726a313bf7706c327e5393c6d096863
mh <man.hx@softworld.vn>
2022-02-28 09:22:47 +0700

    bổ sung thông báo

--- StockDealDal/Dto/EventBus/SuggestTicketDto.cs
+++ StockDealDal/Dto/EventBus/SuggestTicketDto.cs
@@ -11,5 +11,12 @@ namespace StockDealDal.Dto.EventBus
         public Guid TicketId { get; set; }
         public List<Guid> ListReceiverUser { get; set; }
         public Guid UserId { get; set; }
+        public int TicketType { get; set; }
+        public string StockCodes { get; set; }
+        public string Title { get; set; }
+        public int? Quantity { get; set; }
+        public decimal? Price { get; set; }
+        public bool IsNegotiate { get; set; }
+
     }
 }
